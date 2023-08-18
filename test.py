fc570a7900950548473dcb06ff7073245ff40441
Jonathan Ulmer <jonathan.ulmer@ulmail.de>
2023-08-18 15:45:32 +0200

    Implemented FAS solver skeleton and added gitignore

--- test.py
+++ test.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
-import tensorflow as tf
+import numpy as np
 
 
-tf.config.list_physical_devices('GPU')
+class CH_2D_Multigrid_Solver:
+    """
+    Cahn Hillard solever based on the paper [[file:JCP.pdf]]
+
+    """
+
+    def __init__(self):
+        pass
+
+    def SMOOTH(self) -> np.ndarray[(2, 1)]:
+        pass
+
+    def L(self) -> np.ndarray[(2, 1)]:
+        pass
+
+    def v_cycle(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+
+    solver: CH_2D_Multigrid_Solver = CH_2D_Multigrid_Solver()
